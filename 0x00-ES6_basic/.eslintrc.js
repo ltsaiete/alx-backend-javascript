c568ac87a952a945546b5ba8ba9c8889ee7eadc1
Saiete, Luis <lsaiete@deloitte.co.mz>
2023-09-27 20:10:18 +0200

    Fix task 1

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
 	rules: {
 		'no-console': 'off',
 		'no-shadow': 'off',
+		'no-unused-vars': 'off',
 		'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement']
 	},
 	overrides: [
