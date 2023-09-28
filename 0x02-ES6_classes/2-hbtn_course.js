1bf300b87a20dd444328a09f9f2e42bea398aaa3
Saiete, Luis <lsaiete@deloitte.co.mz>
2023-09-29 00:44:34 +0200

    Fix task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Name must be a Number');
     this._name = String(name);
     this._length = Number(length);
     students.forEach((student) => {
+      if (typeof student !== 'string') throw new TypeError('Students must be an array of string');
       this._students.push(String(student));
     });
   }
@@ -11,24 +14,27 @@ export default class HolbertonCourse {
     return this._name;
   }
 
-  set name(value) {
-    this._name = String(value);
+  set name(name) {
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    this._name = String(name);
   }
 
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    this._length = Number(value);
+  set length(number) {
+    if (typeof length !== 'number') throw new TypeError('Name must be a Number');
+    this._length = Number(number);
   }
 
   get students() {
     return this._students;
   }
 
-  set students(values) {
-    values.forEach((student) => {
+  set students(students) {
+    students.forEach((student) => {
+      if (typeof student !== 'string') throw new TypeError('Students must be an array of string');
       this._students.push(String(student));
     });
   }
