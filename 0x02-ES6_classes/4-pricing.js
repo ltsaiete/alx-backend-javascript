fe61c0c789842902ae4b9644c0e420f7f4671496
Saiete, Luis <lsaiete@deloitte.co.mz>
2023-09-29 09:39:39 +0200

    Create static function in task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency';
+import Currency from './3-currency'; // eslint-disable-line
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -26,7 +26,7 @@ export default class Pricing {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
-  convertPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
