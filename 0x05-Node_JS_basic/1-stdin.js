47074d9bcd5979d8a7747297a96d9af15d5fb0c2
Luis Saiete <luistsaiete@gmail.com>
2024-02-25 21:21:50 +0200

    Fix task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,6 +7,6 @@ const reader = readline.createInterface({
 
 reader.question('Welcome to Holberton School, what is your name?\n', (name) => {
   process.stdout.write(`Your name is: ${name}\r`);
-  reader.close();
   console.log('This important software is now closing');
+  reader.close();
 });
