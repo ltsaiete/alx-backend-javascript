60925041bfd83ebd7017c804c81c5f8a1697c6b9
Luis Saiete <luistsaiete@gmail.com>
2024-02-25 23:37:25 +0200

    Read file async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,33 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  return fs.readFile(path, 'utf-8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-
-    const dataArray = data.split('\n');
-    dataArray.pop();
-    dataArray.shift();
-
-    console.log(`Number of students: ${dataArray.length}`);
-
-    const fields = {};
-
-    dataArray.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
-      if (fields[field]) {
-        fields[field].push(firstname);
-      } else {
-        fields[field] = [firstname];
+async function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject('Cannot load the database');
       }
-    });
 
-    Object.keys(fields).forEach((field) => {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      const dataArray = data.split('\n');
+      dataArray.pop();
+      dataArray.shift();
+
+      console.log(`Number of students: ${dataArray.length}`);
+
+      const fields = {};
+
+      dataArray.forEach((student) => {
+        const [firstname, , , field] = student.split(',');
+        if (fields[field]) {
+          fields[field].push(firstname);
+        } else {
+          fields[field] = [firstname];
+        }
+      });
+
+      Object.keys(fields).forEach((field) => {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      });
+      resolve(true);
     });
   });
 }
