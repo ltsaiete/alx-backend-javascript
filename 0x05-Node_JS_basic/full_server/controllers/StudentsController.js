58811a7eaf31d83cca5a1803485b4b8392f51eae
Luis Saiete <luistsaiete@gmail.com>
2024-02-26 12:39:42 +0200

    Fix students display

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,26 +6,24 @@ export class StudentsController {
 
     try {
       const fields = await readDatabase(databasePath);
-      console.log(fields);
 
       Object.keys(fields).forEach((field) => {
         display += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
       });
     } catch (error) {
-      display += 'Cannot load the database';
-      return response.status(500).send(display);
+      return response.status(500).send('Cannot load the database');
     }
 
     return response.send(display);
   }
 
-  static getAllStudentsByMajor(request, response) {
+  static async getAllStudentsByMajor(request, response) {
     const { major } = request.params;
 
     if (major !== 'CS' && major !== 'SWE') return response.status(500).send('Major parameter must be CS or SWE');
 
     try {
-      const fields = readDatabase(databasePath);
+      const fields = await readDatabase(databasePath);
       const display = `List: ${fields[major].join(', ')}`;
       return response.send(display);
     } catch (error) {
--- 0x05-Node_JS_basic/test.js
+++ 0x05-Node_JS_basic/test.js
@@ -1,29 +1,32 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const app = require('./6-http_express');
+process.argv[2] = './database.csv';
+
+import app from './server';
 
 chai.use(chaiHttp);
 chai.should();
 
-describe('Small HTTP server using Express', () => {
-  it('Returns the right content', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((error, response) => {
-        chai.expect(response.text).to.equal('Hello Holberton School!');
-        done();
+describe('Full HTTP server using Express', () => {
+  describe('/students endpoint', () => {
+    describe('When the database is available', () => {
+      before(() => {
+        process.argv[2] = './database.csv';
       });
-  });
 
-  it('Returns the right status', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((error, response) => {
-        chai.expect(response.statusCode).to.equal(200);
-        done();
+      it('Returns the right content', (done) => {
+        chai
+          .request(app)
+          .get('/students')
+          .end((error, response) => {
+            chai.expect(response.statusCode).to.equal(200);
+            chai.expect(response.text).to.have.string(`This is the list of our students
+Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie
+Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy`);
+            done();
+          });
       });
+    });
   });
 });
